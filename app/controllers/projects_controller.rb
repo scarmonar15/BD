424e4b74f87c1789006a0a5a059fafd7566c85ac
Santiago Carmona Rincon <sacarmonar@gmail.com>
2016-05-16 00:48:37 -0500

    all in one

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,101 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+  # GET /projects
+  # GET /projects.json
+  def index
+    @projects = Project.all
+  end
+
+  # GET /projects/1
+  # GET /projects/1.json
+  def show
+  end
+
+  # GET /projects/new
+  def new
+    @project = Project.new
+  end
+
+  # GET /projects/1/edit
+  def edit
+  end
+
+  # POST /projects
+  # POST /projects.json
+  def create
+    @project = Project.new(project_params)
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to @project, notice: 'Project was successfully created.' }
+        format.json { render :show, status: :created, location: @project }
+      else
+        format.html { render :new }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /projects/1
+  # PATCH/PUT /projects/1.json
+  def update
+    respond_to do |format|
+      if @project.update(project_params)
+        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+        format.json { render :show, status: :ok, location: @project }
+      else
+        format.html { render :edit }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /projects/1
+  # DELETE /projects/1.json
+  def destroy
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def differences
+    require 'net/http'
+    url = URI.parse('http://apimasalarms.herokuapp.com/projects.json')
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    api_projects = JSON.parse(res.body)
+    bd_projects = JSON.parse(Project.all.to_json)
+    result = []
+    api_names = []
+    bd_names = []
+    
+    api_projects.map {|ae| api_names << ae["title"]}
+    bd_projects.map {|be| bd_names << be["title"]}
+
+    api_names.each_with_index do |name, index|
+      unless bd_names.include?(name)
+        result << api_projects[index]
+      end
+    end
+    render json: result
+    # result = api_projects - bd_projects
+    # puts "************************************** #{result}"
+
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def project_params
+      params.require(:project).permit(:title, :description)
+    end
+end
